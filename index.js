87888aa2609fde6ba638891ff2c3c7412571dd33
Dave Duran <dave@yodata.io>
2018-06-15 14:03:51 -0700

    fix($dev): fix debug and npm start script

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 require('dotenv').config()
 const Service = require('yodata-inbox-poller')
 const handleMessage = require('./handleMessage')
-const debug = require('debug')
+const debug = require('debug')('rex')
 
 const app = Service.create(process.env.YODATA_INBOX_URL, handleMessage)
+
+app.on('message:process:completed', event => {
+  debug('success',event);
+});
+
+app.on('message:process:failed', event => {
+  debug('error',event);
+});
 
 app.start()
